3586e0f030cad9bf014d2b23c06563adc6a67ca6
Shikhar Bajpai <100301643+SHIKHAR-Bajpai@users.noreply.github.com>
2025-01-03 00:12:11 +0530

    updated config files

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-export const BASE_URL = "http://localhost:8000/api" 
-=======
 export const BASE_URL = "https://packandgo-05jg.onrender.com/api" 
->>>>>>> 2c04f72472a0ebd078555093f2255a59cb9a80be
